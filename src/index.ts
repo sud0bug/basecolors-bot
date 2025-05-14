25e8a568c67226ae56da5bd4e0e0fface731ff56
sud0bug <0xsudobug@gmail.com>
2025-05-14 10:01:26 +0530

    fix: moved setinterval outside so the wss will be unsubscribed regardless

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,14 @@ import {
   RPC,
 } from "./constants.js";
 import { HubRestAPIClient } from "@standard-crypto/farcaster-js-hub-rest";
-import { createPublicClient, webSocket, type Hex, getAddress, parseEventLogs, formatEther } from "viem";
+import {
+  createPublicClient,
+  webSocket,
+  type Hex,
+  getAddress,
+  parseEventLogs,
+  formatEther,
+} from "viem";
 import fetchFCUser from "./getchFCUser.js";
 import { shortenAddressFirstFourLastThree } from "./utils.js";
 import { ethers } from "ethers";
@@ -38,7 +45,12 @@ interface ColorMintPayload {
   address: string;
 }
 
-async function publishToFarcaster(cast: { text: string; url?: string; mentions?: number[]; mentionsPositions?: number[] }) {
+async function publishToFarcaster(cast: {
+  text: string;
+  url?: string;
+  mentions?: number[];
+  mentionsPositions?: number[];
+}) {
   if (!signerPrivateKey || !fid) {
     throw new Error("No signer private key or account fid provided");
   }
@@ -71,11 +83,19 @@ const castToHub = async ({ color, name, address }: ColorMintPayload) => {
     text += `\n\n"${name.toUpperCase()}"`;
   }
 
-  const tweetText = text + `\n\nminted by ${ensName ? ensName : shortenAddressFirstFourLastThree(address)}`;
+  const tweetText =
+    text +
+    `\n\nminted by ${
+      ensName ? ensName : shortenAddressFirstFourLastThree(address)
+    }`;
 
-  text += `\n\nminted by ${fid ? " " : ensName ? ensName : shortenAddressFirstFourLastThree(address)}`;
+  text += `\n\nminted by ${
+    fid ? " " : ensName ? ensName : shortenAddressFirstFourLastThree(address)
+  }`;
 
-  const imageUrl = `https://www.palettes.fun/api/basecolors/image/${color.replace("#", "").toLowerCase()}.png`;
+  const imageUrl = `https://www.palettes.fun/api/basecolors/image/${color
+    .replace("#", "")
+    .toLowerCase()}.png`;
 
   console.log("Casting to Farcaster", {
     text,
@@ -92,7 +112,7 @@ const castToHub = async ({ color, name, address }: ColorMintPayload) => {
   });
 
   await postTweet(tweetText, color);
-}
+};
 
 async function handleTokenMintLogs(data: any, isBatch: boolean = false) {
   console.log("Received token mint log", data);
@@ -110,21 +130,27 @@ async function handleTokenMintLogs(data: any, isBatch: boolean = false) {
 
       console.log("response from alchemy", response);
 
-      const tokenData = await readContract(publicClient, {
+      const tokenData = (await readContract(publicClient, {
         address: CONTRACT_ADDRESS,
         abi: abi,
         functionName: "tokenURI",
-        args: [tokenId]
-      }) as string;
+        args: [tokenId],
+      })) as string;
 
-
-      const parsedTokenData = JSON.parse(Buffer.from(tokenData.replace("data:application/json;base64,", ""), "base64").toString("utf-8"));
+      const parsedTokenData = JSON.parse(
+        Buffer.from(
+          tokenData.replace("data:application/json;base64,", ""),
+          "base64"
+        ).toString("utf-8")
+      );
 
       console.log("parsedTokenData from contract", parsedTokenData);
 
       const color = response.raw.metadata.name;
 
-      const name = parsedTokenData.attributes.find((attribute: any) => attribute.trait_type === "Color Name")?.value;
+      const name = parsedTokenData.attributes.find(
+        (attribute: any) => attribute.trait_type === "Color Name"
+      )?.value;
 
       const payload: ColorMintPayload = {
         color: color,
@@ -137,18 +163,20 @@ async function handleTokenMintLogs(data: any, isBatch: boolean = false) {
 
       castToHub(payload);
     }, 10 * 60 * 1000);
-
   } catch (e) {
     console.error(e);
   }
 }
 
 let lastProcessedAt = Date.now();
+let tokenMinted: {
+  unsubscribe: () => void;
+};
 
 async function createSubscription(address: string) {
   console.log("Creating subscription for address: ", address);
 
-  const tokenMinted = await publicClient.transport.subscribe({
+  tokenMinted = await publicClient.transport.subscribe({
     method: "eth_subscribe",
     params: [
       //@ts-expect-error
@@ -157,26 +185,30 @@ async function createSubscription(address: string) {
         address: [address],
         topics: [
           "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef",
-          "0x0000000000000000000000000000000000000000000000000000000000000000"
+          "0x0000000000000000000000000000000000000000000000000000000000000000",
         ],
       },
     ],
     onData: (data: any) => {
       lastProcessedAt = Date.now();
-      handleTokenMintLogs(data, true)
+      handleTokenMintLogs(data, true);
     },
     onError: (error: any) => {
       console.log("Error in subscription", error);
     },
   });
-
-  const interval = setInterval(() => {
-    if (Date.now() - lastProcessedAt > 60_000 * 5) {
-      clearInterval(interval);
-      tokenMinted.unsubscribe();
-      createSubscription(CONTRACT_ADDRESS);
-    }
-  }, 60_000 * 5);
 }
 
 createSubscription(CONTRACT_ADDRESS);
+const interval = setInterval(() => {
+  if (Date.now() - lastProcessedAt > 60_000 * 5 || !tokenMinted) {
+    try {
+      tokenMinted.unsubscribe();
+    } catch (error) {
+      console.error("Error unsubscribing the wss");
+    }
+    createSubscription(CONTRACT_ADDRESS);
+  }
+}, 60_000 * 5);
+
+// createSubscription(CONTRACT_ADDRESS);
